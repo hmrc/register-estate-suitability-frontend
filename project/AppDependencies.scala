46f4aa1f4d5e406eb88175592ed4d4ab9d1a4c73
Victor Osipov <osipovvictor1994@gmail.com>
2023-07-25 09:45:45 +0100

    DDCE-4085 - Upgrade scala 2.13, + plugins, + Dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,43 +1,30 @@
-import sbt._
+import sbt.*
 
 object AppDependencies {
-  import play.core.PlayVersion
 
-  val compile = Seq(
+  val boostrapVersion = "7.20.0"
+  val mongoVersion = "1.3.0"
+
+  private val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"             % "0.74.0",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "6.5.0-play-28",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.12.0-play-28",
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % "7.13.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"             % mongoVersion,
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "7.15.0-play-28",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.13.0-play-28",
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % boostrapVersion,
   )
 
-  val test = Seq(
-    "uk.gov.hmrc.mongo"           %% "hmrc-mongo-test-play-28"  % "0.74.0",
-    "org.scalatest"               %% "scalatest"                % "3.2.15",
-    "org.scalatestplus.play"      %% "scalatestplus-play"       % "5.1.0",
-    "org.scalatestplus"           %% "scalatestplus-scalacheck" % "3.1.0.0-RC2",
-    "org.jsoup"                   %  "jsoup"                    % "1.15.3",
-    "com.typesafe.play"           %% "play-test"                % PlayVersion.current,
-    "org.mockito"                 %% "mockito-scala-scalatest"  % "1.17.12",
-    "org.scalacheck"              %% "scalacheck"               % "1.17.0",
+  private val test = Seq(
+    "uk.gov.hmrc.mongo"           %% "hmrc-mongo-test-play-28"  % mongoVersion,
+    "org.scalatest"               %% "scalatest"                % "3.2.16",
+    "org.jsoup"                   %  "jsoup"                    % "1.16.1",
+    "org.mockito"                 %% "mockito-scala-scalatest"  % "1.17.14",
+    "org.scalatestplus"           %% "scalacheck-1-17"          % "3.2.16.0",
     "com.github.tomakehurst"      %  "wiremock-standalone"      % "2.27.2",
-    "wolfendale"                  %% "scalacheck-gen-regexp"    % "0.1.2",
-    "com.vladsch.flexmark"        %  "flexmark-all"             % "0.62.2",
-    "uk.gov.hmrc"                 %% "bootstrap-test-play-28"   % "7.13.0",
+    "com.vladsch.flexmark"        %  "flexmark-all"             % "0.64.8",
+    "uk.gov.hmrc"                 %% "bootstrap-test-play-28"   % boostrapVersion,
 
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
 
-  val akkaVersion = "2.6.10"
-  val akkaHttpVersion = "10.1.12"
-
-  val overrides = Seq(
-    "com.typesafe.akka" %% "akka-stream_2.12"     % akkaVersion,
-    "com.typesafe.akka" %% "akka-protobuf_2.12"   % akkaVersion,
-    "com.typesafe.akka" %% "akka-slf4j_2.12"      % akkaVersion,
-    "com.typesafe.akka" %% "akka-actor_2.12"      % akkaVersion,
-    "com.typesafe.akka" %% "akka-http-core_2.12"  % akkaHttpVersion,
-    "commons-codec"     %  "commons-codec"        % "1.12"
-  )
 }
