1ed4b86c4ebb020c2631b85b930835f37f1f2de8
Mark Beresford <mark.beresford@digital.hmrc.gov.uk>
2023-08-31 09:09:53 +0100

    DDCE-4195: Update CSP and some dep updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,13 +2,12 @@ import sbt.*
 
 object AppDependencies {
 
-  val boostrapVersion = "7.20.0"
+  val boostrapVersion = "7.21.0"
   val mongoVersion = "1.3.0"
 
   private val compile = Seq(
-    play.sbt.PlayImport.ws,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"             % mongoVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "7.15.0-play-28",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "7.19.0-play-28",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.13.0-play-28",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % boostrapVersion,
   )
@@ -17,9 +16,9 @@ object AppDependencies {
     "uk.gov.hmrc.mongo"           %% "hmrc-mongo-test-play-28"  % mongoVersion,
     "org.scalatest"               %% "scalatest"                % "3.2.16",
     "org.jsoup"                   %  "jsoup"                    % "1.16.1",
-    "org.mockito"                 %% "mockito-scala-scalatest"  % "1.17.14",
+    "org.mockito"                 %% "mockito-scala-scalatest"  % "1.17.22",
     "org.scalatestplus"           %% "scalacheck-1-17"          % "3.2.16.0",
-    "com.github.tomakehurst"      %  "wiremock-standalone"      % "2.27.2",
+    "org.wiremock"                %  "wiremock-standalone"      % "3.0.0",
     "com.vladsch.flexmark"        %  "flexmark-all"             % "0.64.8",
     "uk.gov.hmrc"                 %% "bootstrap-test-play-28"   % boostrapVersion,
 
