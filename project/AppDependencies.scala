4612e9995829dad53e8bf8d22fb4eecf7abe9843
Mithra Balakrishnan <177737570+mithra-balakrishnan@users.noreply.github.com>
2025-08-11 14:03:56 +0100

    DDCE-6957 - Update deps (#79)
    * DDCE-6957 - Update deps
    
    * DDCE-6957 - removing commented lines
    
    * DDCE-6957 - pr comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,8 +2,8 @@ import sbt.*
 
 object AppDependencies {
 
-  private val boostrapVersion = "9.12.0"
-  private val mongoVersion    = "2.6.0"
+  private val boostrapVersion = "9.19.0"
+  private val mongoVersion    = "2.7.0"
 
   private val compile = Seq(
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % mongoVersion,
@@ -14,8 +14,8 @@ object AppDependencies {
 
   private val test           = Seq(
     "uk.gov.hmrc.mongo"   %% "hmrc-mongo-test-play-30" % mongoVersion,
-    "org.jsoup"            % "jsoup"                   % "1.20.1",
-    "org.scalatestplus"   %% "scalacheck-1-17"         % "3.2.18.0",
+    "org.jsoup"            % "jsoup"                   % "1.21.1",
+    "org.scalatestplus"   %% "scalacheck-1-18"         % "3.2.19.0",
     "uk.gov.hmrc"         %% "bootstrap-test-play-30"  % boostrapVersion
   ).map(_ % Test)
 
