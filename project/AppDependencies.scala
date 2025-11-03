762f6665eabb2b2027a3f33454a960d203c5f673
matthew-gibson-18 <175828706+matthew-gibson-18@users.noreply.github.com>
2025-11-03 08:50:49 +0000

    DDCE-7579 Update repository.yaml to Match Standardized Format

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,12 +2,12 @@ import sbt.*
 
 object AppDependencies {
 
-  private val boostrapVersion = "10.2.0"
+  private val boostrapVersion = "10.4.0"
   private val mongoVersion    = "2.10.0"
 
   private val compile = Seq(
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % mongoVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.18.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.20.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.3.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % boostrapVersion
   )
