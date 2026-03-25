e9e9ed8006fc7f40fd7f1d03f031925228b55c3a
alisajadg191 <34246449+alisajadg191@users.noreply.github.com>
2026-03-25 14:21:45 +0000

    DDC3-8262: Upgrade Bootstrap version and deps

--- build.sbt
+++ build.sbt
@@ -1,4 +1,4 @@
-ThisBuild / scalaVersion := "2.13.16"
+ThisBuild / scalaVersion := "2.13.18"
 ThisBuild / majorVersion := 0
 
 lazy val microservice = Project("register-estate-suitability-frontend", file("."))
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,19 +2,19 @@ import sbt.*
 
 object AppDependencies {
 
-  private val boostrapVersion = "10.5.0"
-  private val mongoVersion    = "2.11.0"
+  private val boostrapVersion = "10.7.0"
+  private val mongoVersion    = "2.12.0"
 
   private val compile = Seq(
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % mongoVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.25.0",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.4.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.32.0",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.5.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % boostrapVersion
   )
 
   private val test = Seq(
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % mongoVersion,
-    "org.scalatestplus" %% "scalacheck-1-18"         % "3.2.19.0",
+    "org.scalatestplus" %% "scalacheck-1-19"         % "3.2.19.0",
     "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % boostrapVersion
   ).map(_ % Test)
 
