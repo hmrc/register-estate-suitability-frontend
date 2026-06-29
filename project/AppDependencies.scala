424bd5cdd04c7987e616d6f0c28467f78022af73
Mithra Balakrishnan <177737570+mithra-balakrishnan@users.noreply.github.com>
2026-06-29 16:28:05 +0100

    DDCE-8573 - Upgrade play-frontend-hmrc (#90)
    * DDCE-8573 - initial commit for play frontend upgrade
    
    * DDCE-8573 - updating play frontend version
    
    * DDCE-8573 - updating back link
    
    * DDCE-8573 - updating sign out
    
    * DDCE-8573 - removing unused attributes
    
    * DDCE-8573 - removing unused attributes

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,14 +7,14 @@ object AppDependencies {
 
   private val compile = Seq(
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % mongoVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.32.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "13.7.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.5.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % boostrapVersion
   )
 
   private val test = Seq(
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % mongoVersion,
-    "org.scalatestplus" %% "scalacheck-1-19"         % "3.2.19.0",
+    "org.scalatestplus" %% "scalacheck-1-19"         % "3.2.20.0",
     "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % boostrapVersion
   ).map(_ % Test)
 
